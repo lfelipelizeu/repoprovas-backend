b74b21d627d03e76ce1dd144a3ce8eb68c0ef630
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-17 23:21:07 -0300

    Add the get tests by professor function at the tests router

--- src/routers/testRouter.ts
+++ src/routers/testRouter.ts
@@ -5,5 +5,6 @@ import * as testController from '../controllers/testController';
 const router = Router();
 
 router.post('/', testController.createTest);
+router.get('/professor/:id', testController.getTestsByProfessor);
 
 export default router;
