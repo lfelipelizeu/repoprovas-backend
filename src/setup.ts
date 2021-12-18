2164b8ec04eb18128b50b5cc5e59073a3ba34e6d
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-17 21:53:12 -0300

    Fix to be right on build

--- src/setup.ts
+++ src/setup.ts
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
 
 let path = '.env';
-if (process.env.NODE_ENV === 'test') path = '.env.test';
-if (process.env.NODE_ENV === 'dev') path = '.env.dev';
+if (process.env.NODE_ENV === 'test') {
+    path = '.env.test';
+}
+if (process.env.NODE_ENV === 'dev') {
+    path = '.env.dev';
+}
 
 dotenv.config({ path });
