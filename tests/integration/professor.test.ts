aaac0e2979718f44923198a47d9e1639644e6ddd
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-19 23:45:28 -0300

    Create an automated test to /professors router

---
+++ tests/integration/professor.test.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-unresolved */
+import { getConnection } from 'typeorm';
+import supertest from 'supertest';
+import app, { init } from '../../src/app';
+
+const agent = supertest(app);
+
+beforeAll(async () => {
+    await init();
+});
+
+describe('GET /professors', () => {
+    it('should returns 200', async () => {
+        const result = await agent.get('/professors');
+        const { status } = result;
+        expect(status).toEqual(200);
+    });
+});
+
+afterAll(async () => {
+    await getConnection().close();
+});
